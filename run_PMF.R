2fe46162e81555db9caca8fb39b042c67a80cf01
Jenna Krall <jenna.krall@gmail.com>
2014-10-28 22:13:58 -0400

    Remove iniParams fn, add censoring adjustment
    Modify code to have cleaner run including having basic
    iniParams.txt file that we modify.  Also added code to
    allow censoring adjustment in runPMF function.

--- run_PMF.R
+++ run_PMF.R
@@ -3,140 +3,95 @@
 #
 
 
+# folder where running these functions from must have
+# 1. iniParams.txt initial parameters
+# 2. PMF_bs2.ini initial file for windows
+# 3. me2wopt.exe ME2 executable
+# and must have write permissions to write .dat files
 
+#requires adjust function from handles package
+library(handles)
 
-###
-# Function to run PMF
-# 
-# dats is list with first element data (nDays X nConstituents)
-# 	and second element uncertainties (same dimension as data)
+#####
+# runPMF takes in complete datafile and uncertainty file
+# and runs PMF using ME2 to get source apportioned results.
 #
-# ndays is number of days
-# ncons is number of chemical constituents (including total)
-outerPMF <- function(dats, nameprev = "nothing.dat", ndays, 
-	ncons, nfact, total = FALSE, 
-	robust = 1, posoutdist = 4,
-	negoutdist = 4, precmode = 20,
-    nruns = 20, numoldsol = 0, bsinitfact = 1,
-    bsmode = 11, simu = 0, contrun = 0,
-    dobspull = 0, pullc1 = 1.5, readbscnts = 0,
-    samplevari = 1, alowlim = -0.2, normc1 = 0.0,
-    acbmodel=0, seed1 = 95, seed2 = 75, seed3 = 75,
-    seed4 = 69, seed5 = 72, c1 = 0, c3 = 0, em = -12,
-    numpf = 0, numynpf = 0, maxpfdim = 20, modelc1 = 1.2,
-    modelc3 = 0.35, pfpullc1 = 1.1, pfsmooc1 = 0.5,
-    doresort = 1, dofpeak = 1){
-  
-  	#create input txt file and run PMF/ME2
-	createiniparams(dats = dats, nameprev = nameprev, 
-		nameout = "testout", iv = "261107",
-        n1 = ndays, n2 = ncons, np = nfact, 
-        robust, posoutdist, negoutdist, precmode,
-        nruns, numoldsol, bsinitfact, bsmode, simu,
-        contrun, dobspull, pullc1, readbscnts, samplevari,
-        alowlim, normc1, acbmodel, seed1, seed2, seed3,
-        seed4, seed5, c1, c3, em,
-        numpf, numynpf, maxpfdim, modelc1, modelc3,
-        pfpullc1, pfsmooc1, doresort, dofpeak)
-  
+# datafile is (path) + (file name as csv)
+# 	(ndays X ncons (with PM, without date))
+# ndays is number of days of observation
+# ncons is number of constituents (including PM if used)
+# nfact is number of sources
+# adjust is censoring adjustment method
+# mdlfile is csv file of mdls corresponding to data
+# uncfile is csv file of uncertainties corresponding to data
+# total indicates whether first column of data is a total
+# seeds are optional seeds to pass to ME2
+runPMF <- function(datafile, ndays, ncons, nfact, adjust = NULL,
+	mdlfile = NULL, uncfile = NULL, total = F, seeds = NULL, pref = "testout") {
+
+    #read in data
+	dat <- read.csv(datafile)
+
+
+
+	#add uncertainty
+	if(!is.null(uncfile)) {
+		unc <- read.csv(uncfile)
+	}else{
+		unc <- matrix(rep(1, nrow(dat) * ncol(dat)), nrow = nrow(dat))
+	}
+	#make total a weak variable according to PMF rules
+	if(total) {
+		unc[, 1] <- unc[, 1] * 3
+	}
+
+
+	#adjust below the MDL if needed (substitute, snrat)
+	if(!is.null(adjust)) {
+		mdl <- read.csv(mdlfile)
+
+		if(class(dat[, 1]) != "Date") {
+			dates <- as.Date(rep(1, nrow(dat)), origin = "1970-01-01")
+			dat <- data.frame(dates, dat)
+		}
+
+		#adjust data removing dates
+		adj <- adjust(dat = dat, mdl = mdl, adjust = adjust, unc = unc)
+		dat <- adj$dat[, -1]
+
+		if(adjust == "snrat") {
+			unc <- adj$unc
+		}
+	}
+
+
+
+
+	#write concatenated data
+	write.table(dat, file = "dat.txt", row.names = F, col.names = F)
+	write.table(unc, file = "dat.txt", row.names = F, col.names = F,
+		append = T)
+
+	#read current initial parameters using PMF default
+	iniParams <- readLines("iniParams.txt")
+
+	#if want to specify seeds directly
+	if(!is.null(seeds)) {
+		iniParams[6] <- paste(seeds, collapse = " ")
+	}
+
+	update number of days, constituents, and sources needed
+	iniParams[7] <- paste(c(ndays, ncons, nfact, 0, 0, -12), collapse = " ")
+	writeLines(iniParams, "iniParams2.txt")
+
+
+	#call ME2 (PMF_bs2 references iniParams.txt for params)
+	system("me2wopt.exe PMF_bs2", show.output.on.console = F, wait = T)
+
 	#interpret results of PMF/ME2
-	solns1 <- readME2("testout", ndays, nfact, ncons, nruns)
-  
-  	#create output
- 	solns <- list(contr = solns1$contr, prof = solns1$prof)
-  	
-  	#if use PM2.5, need to rescale concentrations
-  	if(total == T){
-  		contr <- sweep(solns1$contr, 2, 
-  			as.numeric(solns1$profo[1, ]), "*")
-		solns <- list(contr, solns1$prof)
-	}  
+	solns1 <- readME2(ndays, ncons, nfact, nruns, total, pref)
 
-	solns
-  
-}
-
-
-
-
-
-
-
-
-
-
-###
-# Create input text file and run PMF
-# 
-# dats is list with first element data (nDays X nConstituents)
-# 	and second element uncertainties (same dimension as data)
-# nameprev is previous run (generally ignore) 
-# nameout is name of output file
-# iv is key (generally ignore)
-# n1 is number of days
-# n2 is number of constituents (plus PM25)
-# np is number of sources
-# 
-# There are many other possible inputs.  
-# See ME2 documentation for details
-createiniparams <- function(dats,nameprev="nothing.dat",
-	nameout, iv, n1, n2, np,
-	robust = 1, posoutdist = 4,
-	negoutdist = 4, precmode = 20,
-    numtasks = 20, numoldsol = 0,
-    bsinitfact = 1, bsmode = 11, simu = 0,
-    contrun = 0, dobspull = 0, pullc1 = 1.5,
-    readbscnts = 0, samplevari = 1,
-    alowlim = -0.2, normc1 = 0.0, acbmodel = 0,
-    seed1 = 24, seed2 = 47, seed3 = 54,
-    seed4 = 56, seed5 = 76, c1 = 0, c3 = 0, em = -12,
-    numpf = 0, numynpf = 0, maxpfdim = 20,
-    modelc1 = 1.2, modelc3 = 0.35,
-    pfpullc1 = 1.1, pfsmooc1 = 0.5, doresort = 1,
-    dofpeak = 1){
-  
-	#create lines for text file
-	l1 <- paste(iv)
-	l2 <- paste(robust, posoutdist, negoutdist, 
-		precmode, numtasks, numoldsol, collapse = "\t")
-	l3 <- paste(bsinitfact, bsmode, simu, collapse = "\t")
-	l4 <- paste(contrun, dobspull, pullc1, readbscnts,
-		samplevari, collapse = "\t")
-	l5 <- paste(alowlim, normc1, acbmodel, collapse = "\t")
-	l6 <- paste(seed1, seed2, seed3, seed4, 
-		seed5, collapse = "\t")
-	l7 <- paste(n1, n2, np, c1, c3, em, collapse = "\t")
-	l8 <- paste(numpf, numynpf, maxpfdim, modelc1, modelc3, 
-		pfpullc1, pfsmooc1, collapse = "\t")
-	l10 <- paste(doresort, dofpeak)
-  
- 
-  	#create datafile
-  	write.table(dats[[1]], file = "dat.txt",
-  		row.names = F, col.names = F)
-  	#add uncertainty
-  	write.table(dats[[2]], file = "dat.txt",
-  		row.names = F, col.names = F, append = T)
-
-  
-	nameprev2 <- paste("'", nameprev, "'", sep = "")
-	nameout2 <- paste("'", nameout, "'", sep = "")
-  	l9 <- paste("'dat.txt'", nameprev2, nameout2, 
-  		collapse = "\t")
-  
-  	#create txt file
-	fileo<-file("iniParams.txt")
-  
-  	#write all information
-	writeLines(c(l1, l2, l3, l4, l5, l6, 
-		l7, l8, l9, l10, 0), fileo)
-  
-	close(fileo)
-		
-	#run ME2
-    system("me2wopt.exe PMF_bs2",
-    	show.output.on.console = F, wait = T)
-	
+	return(solns1)
 }
 
 
@@ -149,63 +104,61 @@ createiniparams <- function(dats,nameprev="nothing.dat",
 
 ###
 # Function to apply PMF/ME2
-# 
-# pref is prefix for data
+#
 # ndays is number of days
-# nfact is number of sources
 # ncons is number of constituents (including PM25)
+# nfact is number of sources
 # nruns is number of PMF runs to complete
-readME2 <- function(pref, ndays, nfact, ncons, nruns){
-  
-	#read in data
-	all <- read.table(paste(pref, ".dat", sep= ""),
-		skip = 2, sep = "", strip.white = T)
-  
+# pref is prefix for data
+readME2 <- function(ndays, ncons, nfact, nruns, total = T, pref = "testout"){
+
+	#read in PMF results
+	all <- read.table(paste(pref, ".dat", sep= ""),	skip = 2, sep = "",
+		strip.white = T)
+
   	#check output has correct number of lines
   	neach <- ncons + ndays + 1
-  	if((nruns * neach) != nrow(all)){
+  	if((nruns * neach) != nrow(all)) {
 		print(neach)
   		print(nrow(all))
 
-		browser()
 	}
-  
-  
-  
+
+
+
 	#separate by PMF run, name columns as factors
-	all <- data.frame(rep(seq(1 : nruns),
-		each = neach), all)
-	colnames(all) <- c("nrun", 
-		paste("factor", seq(1 : nfact), sep = ""))
-  
+	all <- data.frame(rep(seq(1 : nruns), each = neach), all)
+	colnames(all) <- c("nrun", paste("factor", seq(1 : nfact), sep = ""))
+
 	#find best PMF run
-	diag <- read.table(paste(pref, ".txt", sep = ""),
-		skip = 2, sep = "", fill = T, 
-		colClasses = c("character"))
+	diag <- read.table(paste(pref, ".txt", sep = ""), skip = 2, sep = "",
+		fill = T, colClasses = c("character"))
 	tasks <- which(diag[, 1] == "task") + 1
 	wrun <- which.min(as.numeric(diag[tasks, 2]))
-  
+
 	#Select best PMF run
 	all <- all[which(all[, 1] == wrun), ]
+
+
+
 	#Get concentrations
 	contr <- all[1 : ndays, -1]
-  
+
 	#Get profiles: remove spacer row
 	prof <- all[(ndays + 1) : (neach - 1), -1]
 	profo <- prof
-  
-  
-	#remove PM25
+
+
+	#remove PM25 and rescale profile
 	prof <- prof[-1, ]
-	#get column sums
-	matcs <- matrix(rep(colSums(prof),
-		nrow(prof)), nrow = nrow(prof),
-		byrow = T)
-	#make sum to 1
-	prof <- prof / matcs
+	prof <- sweep(prof, 2, colSums(prof), "/")
 
 
-	list(contr = contr, prof = prof, 
-  		profo = profo)
+	#if use PM2.5, need to rescale concentrations
+	if(total == T){
+		contr <- sweep(contr, 2, as.numeric(profo[1, ]), "*")
+	}
+
+
+	list(contr = contr, prof = prof)
 }
-
